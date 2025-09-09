395985490cbfdfb4cd4dc2358d2d76076c9da20a
Paul Turner <P_C_Turner@hotmail.com>
2025-09-09 19:28:41 +1000

    initialCommit

---
+++ TravelApp-DotnetMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TravelApp_DotnetMVC.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
